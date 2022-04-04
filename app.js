89fe23d4665dc400bec27aec6129194d4fc8ccba
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-04-04 17:13:31 +0530

    change data by onclick - get user

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const app = Vue.createApp({
     data() {
         return  {
             firstName: 'Dhanesh',
+            lastName: 'Gopalan',
+            email: 'dhaneshg@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    methods: {
+        getUser() {
+            this.firstName = 'Gimisha'
+            this.lastName = 'Gopalan'
+            this.email = 'gimishag@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
         }
     }
     
