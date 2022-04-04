130aa30a176bca1c775cc45e41831270fee6fbcf
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-04-04 17:31:15 +0530

    random user by fetching random user api result shown in async getuser method

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        getUser() {
-            this.firstName = 'Gimisha'
-            this.lastName = 'Gopalan'
-            this.email = 'gimishag@gmail.com'
-            this.gender = 'female'
-            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
-        }
-    }
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+              
+            //console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        },
+    },
     
 })
 
